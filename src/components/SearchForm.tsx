728cf7e9327e3306be541474cfbd8b91f0ae0182
cankskrk <ardacankisakurek@gmail.com>
2025-02-11 23:16:51 +0100

    feat: add SearchForm component

--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,5 +1,27 @@
 const SearchForm = () => {
-  return <div>SearchForm</div>;
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.currentTarget;
+    const searchInput = form.elements.namedItem("search") as HTMLInputElement;
+
+    const searchValue = searchInput?.value;
+
+    if (!searchValue) return;
+
+    console.log(searchValue);
+
+    searchInput.value = "";
+  };
+
+  return (
+    <section>
+      <h1>unsplash images</h1>
+      <form className="" onSubmit={handleSubmit}>
+        <input type="text" name="search" placeholder="cat" />
+        <button>search</button>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
